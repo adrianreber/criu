d73a89265645b4b162b6c6990709d0cef9c44cea
Adrian Reber <areber@redhat.com>
2021-07-19 07:39:51 +0000

    Run 'make indent' on header files
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- criu/include/posix-timer.h
+++ criu/include/posix-timer.h
@@ -8,7 +8,7 @@ struct str_posix_timer {
 	int clock_id;
 	int si_signo;
 	int it_sigev_notify;
-	void * sival_ptr;
+	void *sival_ptr;
 };
 
 struct proc_posix_timer {
@@ -21,7 +21,7 @@ struct proc_posix_timers_stat {
 	struct list_head timers;
 };
 
-extern int parse_posix_timers(pid_t pid, struct proc_posix_timers_stat * args);
+extern int parse_posix_timers(pid_t pid, struct proc_posix_timers_stat *args);
 void free_posix_timers(struct proc_posix_timers_stat *st);
 
 #endif /* __CR_PROC_POSIX_TIMER_H__ */
